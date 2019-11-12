d5a0ea06b6ba92f38ef1aeda1c0b45f990201d61
wangcong <wangcong@medlinker.com>
2019-11-12 15:36:40 +0800

    Observer 事件观察者

--- RxStudy.playground/Pages/Observer.xcplaygroundpage/Contents.swift
+++ RxStudy.playground/Pages/Observer.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: [Previous](@previous)
 
-/// 本Demo介绍Observer，由于Observer并非像Observable那样种类多且负责，故在此将特征Observer与非特征Observer一并介绍
+/// 本Demo主要介绍Observer，由于Observer并非像Observable那样种类多且负责，故在此将特征Observer与非特征Observer一并介绍
 
 import Foundation
 import RxSwift
